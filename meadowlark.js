08d4e94bf9c374dd8fa555c7321ddf6473fdfdab
ncorrare <ncorrare@gmail.com>
2014-08-11 14:38:42 +0100

    Moved 'fortune cookie' functionality into module.

---
+++ meadowlark.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var fortune = require('./lib/fortune.js');
+var app = express();
+var handlebars = require('express3-handlebars').create({ defaultLayout:'main'});
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req,res){
+	res.render('home');
+});
+app.get('/about', function(req,res){
+	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', { fortune: fortune.getFortune });
+});
+
+app.use(function(req,res){
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:'+ app.get('port') +'; press Ctrl-C to terminate.');
+});
